1c1fd28f43b4b79afe39d8be83d96ae9558888f5
sigurdurhaukur <sigurdur.h.b@icloud.com>
2020-11-13 16:28:12 +0000

    added more "bling"

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-xwindow.addEventListener('scroll', function(e) {
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
 
-    const target = document.querySelectorAll('.scroll');
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
 
-
-    var index = 0, length = target.length;
-    for (index; index < length; index++) {
-        var pos = window.pageYOffset * target[index].dataset.rate;
-
-        if(target[index].dataset.direction === 'vertical') {
-            target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
-        } else {
-            var posX = window.pageYOffset * target[index].dataset.ratex;
-            var posY = window.pageYOffset * target[index].dataset.ratey;
-            
-            target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
-        }
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
     }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
 });
