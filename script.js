6be80b6abd3023f88afb60e5289088a2f5907b2f
sigurdurhaukur <sigurdur.h.b@icloud.com>
2020-11-13 16:44:20 +0000

    resurected the parrallax effect 👌😈

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// parrallax
+window.addEventListener("scroll", function (e) {
+  const target = document.querySelectorAll(".scroll");
+
+  var index = 0,
+    length = target.length;
+  for (index; index < length; index++) {
+    var pos = window.pageYOffset * target[index].dataset.rate;
+
+    if (target[index].dataset.direction === "vertical") {
+      target[index].style.transform = "translate3d(0px," + pos + "px, 0px)";
+    } else {
+      var posX = window.pageYOffset * target[index].dataset.ratex;
+      var posY = window.pageYOffset * target[index].dataset.ratey;
+
+      target[index].style.transform =
+        "translate3d(" + posX + "px, " + posY + "px, 0px)";
+    }
+  }
+});
+
+// fade / slide in
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
 
